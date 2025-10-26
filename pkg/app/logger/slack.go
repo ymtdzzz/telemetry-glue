2230802ff7a08a32df63cbf41f398e9783aa2728
Y.Matsuda <ymatsuda.zck@gmail.com>
2025-10-26 21:39:14 +0900

    fix: use thread_ts instead of response_url in slack client

--- pkg/app/logger/slack.go
+++ pkg/app/logger/slack.go
@@ -4,16 +4,16 @@ import "github.com/slack-go/slack"
 
 // SlackLogger is a logger that sends logs to a Slack channel
 type SlackLogger struct {
-	client      *slack.Client
-	channelID   string
-	responseURL string
+	client    *slack.Client
+	channelID string
+	threadTS  string
 }
 
-func NewSlackLogger(client *slack.Client, channelID, responseURL string) *SlackLogger {
+func NewSlackLogger(client *slack.Client, channelID, threadTS string) *SlackLogger {
 	return &SlackLogger{
-		client:      client,
-		channelID:   channelID,
-		responseURL: responseURL,
+		client:    client,
+		channelID: channelID,
+		threadTS:  threadTS,
 	}
 }
 
@@ -21,7 +21,7 @@ func (l *SlackLogger) Log(message string) error {
 	_, _, err := l.client.PostMessage(
 		l.channelID,
 		slack.MsgOptionText(message, false),
-		slack.MsgOptionResponseURL(l.responseURL, "in_channel"),
+		slack.MsgOptionTS(l.threadTS),
 	)
 	return err
 }
